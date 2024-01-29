ceab8f3cff79c2cc7cd3d87300388c062ed12454
bimal-atcs <bimal.rana@atcs.com>
2024-01-29 21:13:26 +0530

    updated service core and hero banner

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,19 @@
 //run when page first loads
 $(document).ready(function () {
     pageContent();
+
+    $(".hero-slider").slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: false,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
 });
 
 //run on every window resize
