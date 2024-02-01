e2e52c7f12fb1fdece811994986a726244c0fc83
bimal-atcs <bimal.rana@atcs.com>
2024-02-01 20:47:35 +0530

    updated mega menu

---
+++ assets/js/header.js
@@ -0,0 +1,12 @@
+//run when page first loads
+$(document).ready(function () {
+    headerMenu();
+});
+
+//run on every window resize
+$(window).resize(function () {
+    headerMenu();
+});
+
+function headerMenu() {
+}
--- assets/js/home.js
+++ assets/js/home.js
@@ -5,12 +5,11 @@ $(document).ready(function () {
     $(".hero-slider").slick({
         dots: true,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 7000,
         arrows: false,
         slidesToShow: 1,
         slidesToScroll: 1,
         draggable: false,
-        speed: 500,
         fade: true,
         cssEase: 'linear'
     });
