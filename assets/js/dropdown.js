d0876243bb12740aa7ced202bacc82214ac1a999
bimal-atcs <bimal.rana@atcs.com>
2024-02-06 19:43:12 +0530

    updated scp page

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -10,21 +10,30 @@ $(window).resize(function () {
 
 
 function dropdownSCP() {
+    // dropdown label
     $('.dropdown-option .dropdown-label').click(function(e) {
         e.preventDefault();
         $(this).parents('.dropdown-option').toggleClass('dropdown-select-open');
     });
 
+    // dropdowm sub items
     $('.dropdown-view-list li > .dropdown-view-label').click(function(e) {
         e.preventDefault();
         $(this).toggleClass('dropdown-view-label-active');
     });
 
+    // switcher
+    $('.switch').click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('switch-active');
+    });
+
+    // dropdown clear
     $('.dropdown-clear').click(function(e) {
         e.preventDefault();
         $('.dropdown-select:not(.dropdown-clear)').removeClass('dropdown-select-open');
         $('.dropdown-view-list li > .dropdown-view-label').removeClass('dropdown-view-label-active');
-        $('.switch').prop("checked", false);
+        $('.switch').removeClass('switch-active');
         $(this).removeClass('dropdown-clear-active');
     });
 }
