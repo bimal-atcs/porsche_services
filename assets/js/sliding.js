b5773bae3cb5260a98294b651b78f58599062787
bimal-atcs <bimal.rana@atcs.com>
2024-02-08 02:55:59 +0530

    updated scp pages

--- assets/js/sliding.js
+++ assets/js/sliding.js
@@ -1,3 +1,49 @@
+//run when page first loads
+$(document).ready(function () {
+    sliding();
+    infoHover();
+});
+
+//run on every window resize
+$(window).resize(function () {
+    sliding();
+    infoHover();
+});
+
+// sliding items height
+function sliding() {
+    $slideHeight = $('.flickity-viewport').css('height');
+    $('.sliding-item').css('height', $slideHeight);
+}
+
+// info hover popup
+function infoHover() {
+    $winWidth = $(window).width();
+
+    $('.sliding-card').mouseenter(function(e) {
+        e.preventDefault();
+        $(this).parents(".sliding-item").css({
+            'z-index': '99'
+        });
+        $(this).find(".info-block").addClass('info-block-show');
+
+        $slideCardThis = $(this).offset().left;
+        if($slideCardThis < ($winWidth/2)) {
+            $(".info-block").removeClass('info-block-right');
+            $(this).find(".info-block").addClass('info-block-left');
+        }
+        else {
+            $(".info-block").removeClass('info-block-left');
+            $(this).find(".info-block").addClass('info-block-right');
+        }
+    }).mouseleave(function(e) {
+        e.preventDefault();
+        $(this).parents(".sliding-item").css('z-index', '');
+        $(".info-block").removeClass('info-block-show');
+    });
+}
+
+// sliding drag scroll
 function is_touch_device() {
     return 'ontouchstart' in window || navigator.maxTouchPoints;
 }
