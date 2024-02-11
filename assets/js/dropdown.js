02cc1531e8bad24647246230ec63987569da7c64
bimal-atcs <bimal.rana@atcs.com>
2024-02-12 02:07:27 +0530

    updated service page

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,13 +1,11 @@
 //run when page first loads
 $(document).ready(function () {
     dropdownDown();
-    dropdownSCP();
 });
 
 //run on every window resize
 $(window).resize(function () {
     dropdownDown();
-    dropdownSCP();
 });
 
 
@@ -20,34 +18,5 @@ function dropdownDown() {
         } else if (!$(this).hasClass("dropdown-active")) {
             $(this).addClass("dropdown-active");
         }
-    });
-}
-
-function dropdownSCP() {
-    // dropdown label
-    $('.dropdown-option .dropdown-label').click(function(e) {
-        e.preventDefault();
-        $(this).parents('.dropdown-option').toggleClass('dropdown-select-open');
-    });
-
-    // dropdowm sub items
-    $('.dropdown-view-list li > .dropdown-view-label').click(function(e) {
-        e.preventDefault();
-        $(this).toggleClass('dropdown-view-label-active');
-    });
-
-    // switcher
-    $('.switch').click(function(e) {
-        e.preventDefault();
-        $(this).toggleClass('switch-active');
-    });
-
-    // dropdown clear
-    $('.dropdown-clear').click(function(e) {
-        e.preventDefault();
-        $('.dropdown-select:not(.dropdown-clear)').removeClass('dropdown-select-open');
-        $('.dropdown-view-list li > .dropdown-view-label').removeClass('dropdown-view-label-active');
-        $('.switch').removeClass('switch-active');
-        $(this).removeClass('dropdown-clear-active');
     });
 }
---
+++ assets/js/dropdownComp.js
@@ -0,0 +1,67 @@
+//run when page first loads
+$(document).ready(function () {
+    dropdownComp();
+});
+
+//run on every window resize
+$(window).resize(function () {
+});
+
+function dropdownComp() {
+    // dropdowm sub items
+    $('.dropdown-view-label').click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('dropdown-view-label-active');
+    });
+
+    // switcher
+    $('.switch').click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('switch-active');
+    });
+
+    // aside dropdown option
+    if ($('.aside-dropdown').hasClass('dropdown-option')) {
+        $('.aside-dropdown .aside-button').click(function(e) {
+            e.preventDefault();
+            $(this).parents('.aside-dropdown').toggleClass('dropdown-option-active');
+        });
+    } else {
+        $('.dropdown-option').removeClass('.dropdown-option-active');
+    }
+    
+    // select dropdown label
+    if ($('.dropdown-select').hasClass('dropdown-option')) {
+        $('.dropdown-select .dropdown-label').click(function(e) {
+            e.preventDefault();
+            $(this).parents('.dropdown-select').toggleClass('dropdown-select-open');
+            if ($('.dropdown-select').hasClass('dropdown-select-open')) {
+                $('.dropdown-select-clear').addClass('dropdown-select-clear-active');
+            } else {
+                $('.dropdown-select-clear').removeClass('dropdown-select-clear-active');
+            }
+        });
+    } else {
+        $('.dropdown-option').removeClass('.dropdown-select-open');
+    }
+
+    // dropdown clear
+    if ($('.dropdown-option').find('.dropdown-clear')) {
+        $('.dropdown-clear').click(function(e) {
+            e.preventDefault();
+            $('.dropdown-view-label').removeClass('dropdown-view-label-active');
+            $('.switch').removeClass('switch-active');
+        });
+    }
+
+    // dropdown select clear
+    if ($('.dropdown-select').hasClass('dropdown-select-clear')) {
+        $('.dropdown-select-clear').click(function(e) {
+            e.preventDefault();
+            $('.dropdown-select').removeClass('dropdown-select-open');
+            $('.dropdown-view-label').removeClass('dropdown-view-label-active');
+            $('.switch').removeClass('switch-active');
+            $(this).removeClass('dropdown-select-clear-active');
+        });
+    }
+}
